41e204b48b015dbd036df71583b3e53c51102830
zhoujingfighting <254644528@qq.com>
2022-11-12 21:53:12 +0800

    Update patch.sh

---
+++ utils/patch.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/sh
+
+PWD=`cd $(dirname $0); pwd -P`
+if [ -n "${1}" ];then
+    REACT_BUILD_DIR=${1}
+else
+    REACT_BUILD_DIR=${PWD}
+fi
+
+if [ ! -d "${REACT_BUILD_DIR}" ];then
+    echo "Source directory does not exit, please check !"
+else
+    echo "*****Start cloning*****"
+fi
+
+cd ${REACT_BUILD_DIR}
+git clone https://github.com/facebook/react
+
+# Patch build.js
+
+cp -f ${PWD}/build.js ${REACT_BUILD_DIR}/scripts/rollup/build.js
+
+
+#Build source code
+
+NODE_EX_PATH=`which node`
+if [ -z "${NODE_EX_PATH}" ];then
+    echo "Please install node first"
+    exit 1
+fi
+
+node scripts/rollup/build.js
+
