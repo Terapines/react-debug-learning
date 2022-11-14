dd962ca4b48ae560f0cf8274a078515605edbceb
zhoujing <jing.zhou@terapines.com>
2022-11-14 17:26:00 +0800

    Update Patch.sh

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -751,9 +751,5 @@ module.exports = function (webpackEnv) {
     // Turn off performance processing because we utilize
     // our own hints via the FileSizeReporter
     performance: false,
-    externals: {
-        react: "React",
-        'react-dom':"ReactDom"
-    },
   };
 };
--- utils/patch.sh
+++ utils/patch.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/sh
 
+ROOT_DIR=$(cd $(dirname $0); pwd)
 PWD=`cd $(dirname $0); pwd -P`
 if [ -n "${1}" ];then
     REACT_BUILD_DIR=${1}
 else
-    REACT_BUILD_DIR=${PWD}
+    REACT_BUILD_DIR="$HOME"
 fi
 
 if [ ! -d "${REACT_BUILD_DIR}" ];then
-    echo "Source directory does not exit, please check !"
+    # This may cause problems on Windows 
+    echo "Clone Source directory does not exit, please check !"
+    exit 1
 else
     echo "*****Start cloning*****"
 fi
 
 cd ${REACT_BUILD_DIR}
-git clone -b 18.2 https://github.com/facebook/react
+git clone https://github.com/facebook/react
 
 # Patch build.js
 if [ ! -d ${REACT_BUILD_DIR}/scripts/rollup/ ];then
     mkdir -p ${REACT_BUILD_DIR}/scripts/rollup/
 fi
 
-cp -f ${PWD}/build.js ${REACT_BUILD_DIR}/scripts/rollup/
+cp -f ${ROOT_DIR}/build.js ${REACT_BUILD_DIR}/react/scripts/rollup/
 
 
 #Build source code
@@ -32,6 +35,6 @@ if [ -z "${NODE_EX_PATH}" ];then
     exit 1
 fi
 
+npm i -g rollup
 cd  ${REACT_BUILD_DIR}/react
-npm install && node ${REACT_BUILD_DIR}/react/scripts/rollup/build.js
-
+node ${REACT_BUILD_DIR}/react/scripts/rollup/build.js
